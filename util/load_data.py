875b3a419b2db218d477a25a7a5df57e5a8ee676
Youngrok Song <id2thomas@gmail.com>
2020-09-24 21:37:27 +0900

    added data split, filter functions

--- util/load_data.py
+++ util/load_data.py
@@ -1,32 +1,35 @@
 import os
 import h5py
+
 import random
 import numpy as np
 import torch
 
 
-def get_dataset(dirname=os.path.join('hdf5'), filename=None, tagname=None):
-    if filename is None or tagname is None:
-        raise ValueError('File name and tag name should be set.')
-    with h5py.File(os.path.join(dirname, filename), 'r') as hdf:
-        return hdf[tagname][:]
+def get_hdf5_data(file_path,labeled=False):
+    with h5py.File(file_path,'r') as f:
+        data=f['x'].value
+        if labeled:
+            label=f['y'].value
+        else:
+            label=[]
+    return data,label
 
+def split_data(data,label,split_ratio=0.1,seed_num=42):
+    X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=split_ratio, random_state=seed_num)
+    return X_train, X_test, y_train, y_test
 
+def filter_label(data,label,select_label=0):
+    select_idx=[label==select_label]
+    print("Selected {} total {}".format(data[select_idx].shape[0],data.shape[0]))
+    data=data[tuple(select_idx)]
+    label=label[tuple(select_idx)]
+    return data,label
+    
 def set_seed(seed_num):
     random.seed(seed_num)
     np.random.seed(seed_num)
     torch.manual_seed(seed_num)
     torch.cuda.manual_seed_all(seed_num)
-
-
-def get_hdf5_data(dirpath):
-    hdf5_files = os.listdir(dirpath)
-    print(dirpath)
-    data = []
-    label = []
-    for hdf5_file in hdf5_files:
-        with h5py.File(dirpath+'/'+hdf5_file,'r') as f:
-            data.append(f['data'].value)
-            label.append(f['label'].value)
-        print(hdf5_file)
-    return np.concatenate(data), np.concatenate(label).flatten()
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
